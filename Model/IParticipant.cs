515ef12e9c64ccf93fd8af814fd0fc0e17e81f0f
Mike Thomas <mike.thomas2402@gmail.com>
2020-09-10 16:44:27 +0200

    Make IParticipant interface and TeamColors enum

---
+++ Model/IParticipant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    enum TeamColors
+    {
+        Red,
+        Green,
+        Yellow,
+        Grey,
+        Blue
+    }
+
+    interface IParticipant
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public IEquipment Equipment { get; set; }
+        public TeamColors TeamColor { get; set; }
+    }
+}
