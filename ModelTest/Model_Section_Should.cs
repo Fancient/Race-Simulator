2af2ed2b2d28667ecd7774efb16d594e686ddd5b
Mike Thomas <mike.thomas2402@gmail.com>
2020-09-16 20:05:13 +0200

    Unit Tests for Sections class

---
+++ ModelTest/Model_Section_Should.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Model;
+
+namespace ModelTest
+{
+    [TestFixture]
+    class Model_Section_Should
+    {
+        [Test]
+        public void Section_Object_NotNull()
+        {
+            Section section = new Section(SectionTypes.Straight);
+
+            Assert.NotNull(section);
+        }
+
+        [Test]
+        public void Section_SectionType_ShouldReturnEqual()
+        {
+            Section section = new Section(SectionTypes.Straight);
+
+            var actual = section.SectionType;
+
+            Assert.AreEqual(SectionTypes.Straight, actual);
+        }
+
+        [Test]
+        public void Section_SectionType_SetSection_ShouldReturnEqual()
+        {
+            Section section = new Section(SectionTypes.Straight);
+
+            section.SectionType = SectionTypes.Finish;
+            var actual = section.SectionType;
+
+            Assert.AreEqual(SectionTypes.Finish, actual);
+        }
+    }
+}
