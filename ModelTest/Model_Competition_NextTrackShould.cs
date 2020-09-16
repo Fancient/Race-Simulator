45317b9add5070f8e57027029e2ccc497121aabe
Mike Thomas <mike.thomas2402@gmail.com>
2020-09-16 16:15:45 +0200

    Add unit test

---
+++ ModelTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace ModelTest
+{
+    [TestFixture]
+    class Model_Competition_NextTrackShould
+    {
+        private Competition _competition;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _competition = new Competition();
+        }
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            var result = _competition.NextTrack();
+
+            Assert.IsNull(result);
+        }
+    }
+}
