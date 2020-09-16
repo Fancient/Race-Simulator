7f46b3046a468e4cab86c103f505871f4c1e171c
Mike Thomas <mike.thomas2402@gmail.com>
2020-09-16 19:23:04 +0200

    add more tests for NextTrack()

--- ModelTest/Model_Competition_NextTrackShould.cs
+++ ModelTest/Model_Competition_NextTrackShould.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 using Model;
 
 namespace ModelTest
@@ -28,11 +29,42 @@ namespace ModelTest
         [Test]
         public void NextTrack_OneInQueue_ReturnTrack()
         {
-            Track track = new Track("Track", new SectionTypes[]{});
+            Track track = new Track("Track", new SectionTypes[] { });
             _competition.Tracks.Enqueue(track);
             var result = _competition.NextTrack();
 
             Assert.AreEqual(track, result);
+        }
+
+        [Test]
+        public void NextTrack_OneInQueue_RemoveTrackFromQueue()
+        {
+            Track track = new Track("Track", new SectionTypes[] { });
+            _competition.Tracks.Enqueue(track);
+
+            var result = _competition.NextTrack();
+            result = _competition.NextTrack();
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void NextTrack_TwoInQueue_ReturnNextTrack()
+        {
+            // this test will assert if the NextTrack method properly returns the next in queue
+            // create tracks
+            Track track1 = new Track("Track1", new SectionTypes[] { });
+            Track track2 = new Track("Track2", new SectionTypes[] { });
+
+            // enqueue tracks
+            _competition.Tracks.Enqueue(track1);
+            _competition.Tracks.Enqueue(track2);
+
+            // dequeue tracks twice, expect track 2 in result
+            var result = _competition.NextTrack();
+            result = _competition.NextTrack();
+
+            Assert.AreEqual(track2, result);
         }
     }
 }
