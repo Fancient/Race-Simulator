24d5afa0500014d35180a8b374c760110401ec05
Mike Thomas <mike.thomas2402@gmail.com>
2020-09-21 12:18:02 +0200

    Visualisation concept working

--- ConsoleEdition/Visualization.cs
+++ ConsoleEdition/Visualization.cs
@@ -7,29 +7,63 @@ namespace ConsoleEdition
 {
     public enum Direction
     {
-        UP,
-        LEFT,
-        DOWN,
-        RIGHT
+        N,
+        E,
+        S,
+        W
     }
     public static class Visualization
     {
         private const int _cursorStartPosX = 24;
         private const int _cursorStartPosY = 12;
 
-        private static int _currentPosX = _cursorStartPosX;
-        private static int _currentPosY = _cursorStartPosY;
+        private static int _cPosX = _cursorStartPosX;
+        private static int _cPosY = _cursorStartPosY;
 
         // tracks always start pointing right.
-        private static Direction _currentDirection = Direction.RIGHT;
+        private static Direction _currentDirection = Direction.E;
 
         #region graphics
-
         private static string[] _finishHorizontal = { "----", "  # ", "  # ", "----" };
-        private static string[] _finishVertical = { "----", "  # ", "  # ", "----" };
         private static string[] _startGridHorizontal = { "----", "  ] ", "  ] ", "----" };
 
+        private static string[] _straightHorizontal = {"----", "    ", "    ", "----"};
+        private static string[] _straightVertical = {"|  |", "|  |", "|  |", "|  |"};
+
+        private static string[] _cornerNE = {" /--", "/   ", "|   ", "|  /"};
+        private static string[] _cornerNW = {@"--\ ", @"   \", @"   |", @"\  |"};
+        private static string[] _cornerSE = {@"|  \", @"|   ", @"\   ", @" \--"};
+        private static string[] _cornerSW = { "/  |", "   |", "   /", "--/ " };
         #endregion
+
+        public static string[] SectionTypeToGraphic(SectionTypes sectionType, Direction direction)
+        {
+            return sectionType switch
+            {
+                SectionTypes.Straight => ((int)direction % 2) switch
+                {
+                    0 => _straightVertical,
+                    1 => _straightHorizontal
+                },
+                SectionTypes.LeftCorner => (int)direction switch
+                {
+                    0 => _cornerNW,
+                    1 => _cornerSW,
+                    2 => _cornerSE,
+                    3 => _cornerNE
+                },
+                SectionTypes.RightCorner => (int)direction switch
+                {
+                    0 => _cornerNE,
+                    1 => _cornerNW,
+                    2 => _cornerSW,
+                    3 => _cornerSE
+                },
+                SectionTypes.StartGrid => _startGridHorizontal,
+                SectionTypes.Finish => _finishHorizontal
+            };
+        }
+
         public static void Initialize()
         {
             // initialize some stuff
@@ -38,29 +72,78 @@ namespace ConsoleEdition
         public static void DrawTrack(Track track)
         {
             // to test, first draw string.
-            DrawSingleSection(_finishHorizontal, ref _currentPosX, ref _currentPosY, ref _currentDirection);
-            DrawSingleSection(_finishHorizontal, ref _currentPosX, ref _currentPosY, ref _currentDirection);
-            DrawSingleSection(_finishHorizontal, ref _currentPosX, ref _currentPosY, ref _currentDirection);
+            foreach (Section trackSection in track.Sections)
+            {
+                DrawSingleSection(trackSection);
+            }
         }
 
-        public static void DrawSingleSection(string[] sectionStrings, ref int cursorX, ref int cursorY, ref Direction direction)
+
+        public static void DrawSingleSection(Section section)
         {
-            // steps to do:
-            // start printing given section
-            // prepare cursor position for next section.
-            // while preparing for next section, take rotation of track into account.
-            // (maybe direction variable?) ((or enum))
+            // first determine section string
+            string[] sectionStrings = SectionTypeToGraphic(section.SectionType, _currentDirection);
+
+            // print section
+            int tempY = _cPosY;
             foreach (string s in sectionStrings)
             {
-                Console.SetCursorPosition(cursorX, cursorY);
+                Console.SetCursorPosition(_cPosX,tempY);
                 Console.Write(s);
-                cursorY++;
+                tempY++;
             }
 
-            cursorY -= 4;
-            cursorX += 4;
+            // flip direction if corner piece
+            if (section.SectionType == SectionTypes.RightCorner)
+                _currentDirection = ChangeDirectionRight(_currentDirection);
+            else if (section.SectionType == SectionTypes.LeftCorner)
+                _currentDirection = ChangeDirectionLeft(_currentDirection);
+
+            // change cursor position based on current.
+            ChangeCursorToNextPosition();
 
         }
 
+        public static Direction ChangeDirectionLeft(Direction d)
+        {
+            return d switch
+            {
+                Direction.N => Direction.W,
+                Direction.E => Direction.N,
+                Direction.S => Direction.E,
+                Direction.W => Direction.S
+            };
+        }
+
+        public static Direction ChangeDirectionRight(Direction d)
+        {
+            return d switch
+            {
+                Direction.N => Direction.E,
+                Direction.E => Direction.S,
+                Direction.S => Direction.W,
+                Direction.W => Direction.N
+            };
+        }
+
+
+        public static void ChangeCursorToNextPosition()
+        {
+            switch (_currentDirection)
+            {
+                case Direction.N:
+                    _cPosY -= 4;
+                    break;
+                case Direction.E:
+                    _cPosX += 4;
+                    break;
+                case Direction.S:
+                    _cPosY += 4;
+                    break;
+                case Direction.W:
+                    _cPosX -= 4;
+                    break;
+            }
+        }
     }
 }
