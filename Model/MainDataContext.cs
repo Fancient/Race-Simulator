932e6bcb0eb8e1872a8d34b85a454eceed81945e
Mike Thomas <mike.thomas2402@gmail.com>
2020-10-19 12:05:45 +0200

    Add DataContext

---
+++ Model/MainDataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Model
+{
+    public class MainDataContext : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainDataContext(EventHandler<DriversChangedEventArgs> DriversChangedEventHandler)
+        {
+            DriversChangedEventHandler += OnDriversChanged;
+        }
+
+        public MainDataContext()
+        {
+
+        }
+
+        public void OnDriversChanged(object sender, DriversChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
+        }
+    }
+}
